82c71ab445329eed71259221e1f77bf0bb51425a
OneDeadL1ne <mikemp201@gmail.com>
2023-10-10 15:37:53 +0300

    add grid doc

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,14 @@
 import CustomMap from "@/components/CustomMap";
+import { GridDoc } from "@/components/GridDoc";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div>здесь будет порядок докуметнов</div>
+      <div>
+        <GridDoc />
+      </div>
 
       <div>таблица админка + редактирование</div>
     </main>
---
+++ my-app/components/GridDoc.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function GridDoc() {
+  return <div>GridDoc</div>;
+}
