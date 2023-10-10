f50ee366295758ecf12042492a50d87c3ed60fd1
Yuka <maxzerovol@gmail.com>
2023-10-10 17:33:54 +0300

    ufyk
    yu

--- my-app/components/GridDoc.tsx
+++ my-app/components/GridDoc.tsx
@@ -3,9 +3,10 @@
 export function GridDoc() {
   return <div>
     <div className="grid grid-cols-3 ">
-    <div className="border-solid border-[1px] border-black text-center text-5xl p-10">ljrevtyn yf</div>
-    <div className="border-solid border-[1px] border-black text-center text-5xl p-10">ljrevtyn yf</div>
-    <div className="border-solid border-[1px] border-black text-center text-5xl p-10">ljrevtyn yf</div>
+    <div className="border-solid border-[1px] border-[#00000044] text-center text-2xl p-10">ljrevtyn yf</div>
+    <div className="border-solid border-[1px] border-[#00000044] text-center text-2xl p-10">ljrevtyn yf</div>
+    <div className="border-solid border-[1px] border-[#00000044] text-center text-2xl p-10">ljrevtyn yf</div>
+    
 
 
     </div>
