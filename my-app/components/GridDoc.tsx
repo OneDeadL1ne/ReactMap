ac85780cca3a6abd00bee42601e0d66345769683
Yuka <maxzerovol@gmail.com>
2023-10-10 15:52:43 +0300

    ert

--- my-app/components/GridDoc.tsx
+++ my-app/components/GridDoc.tsx
@@ -1,5 +1,13 @@
 "use client";
 
 export function GridDoc() {
-  return <div>GridDoc</div>;
+  return <div>
+    <div className="grid grid-cols-3 ">
+    <div className="border-solid border-[1px] border-black text-center text-6xl m-11">ljrevtyn yf</div>
+    <div className="border-solid border-[1px] border-black">ljrevtyn yf</div>
+    <div className="border-solid border-[1px] border-black">ljrevtyn yf</div>
+
+
+    </div>
+  </div>;
 }
